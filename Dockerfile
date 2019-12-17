4c29a4f8fe8f5b7c00c5c9a8df3f2abe049e9fa8
Augie <augie@luebbers.email>
2019-12-17 15:54:33 -0600

    Added translation service

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
-FROM node:10-alpine
+FROM nikolaik/python-nodejs:latest
 RUN mkdir -p /home/node/uwu/node_modules && chown -R node:node /home/node/uwu
 WORKDIR /home/node/uwu
 COPY package*.json ./
-COPY config.json ./
 USER node
 RUN npm install
 COPY --chown=node:node . .
-CMD ["node","app.js"]
+CMD ["sh", "-c", "node","app.js"]
--- app.js
+++ app.js
@@ -10,11 +10,40 @@ client.on("ready", () => {
 client.on("message", (message) => {
 
     if(!message.author.bot) {
-        if(message.content.toLowerCase().includes('uwu')) {
+        if(message.content.startsWith('!uwu')) {
+            translate(message);
+        } else if(message.content.toLowerCase().includes('uwu')) {
             message.channel.send('uwu');
         }
     }
 
 });
 
-client.login(config.token);
+const translate = ((message) => {
+    fetchMessage(message).then(data => {
+        let spawn = require("child_process").spawn;
+        let process = spawn('python',["./translate.py", data.content]);
+        process.stdout.on('data', (data) => { 
+            message.channel.send(data.toString());
+        }); 
+    
+    });
+});
+
+const fetchMessage = (message) => {
+    return new Promise(resolve => {
+        message.channel.fetchMessages({ limit: 10 })
+        .then(messages => {
+            // Cast messages to an array
+            messages = Array.from(messages.values());
+            for (let i = 0; i < messages.length; i++) {
+                if (messages[i].id === message.id) {
+                    resolve(messages[i+1]);
+                }
+            }
+        })
+        .catch(console.error);
+    });
+}
+
+client.login(process.env.token);

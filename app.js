0d5b1ecb3219c5f722847eee67280dfca47f0662
Augie <augie@luebbers.email>
2019-12-16 22:49:51 -0600

    uwu

---
+++ app.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+const client  = new Discord.Client();
+const config = require('./config.json');
+client.on("ready", () => {
+    console.log("uwu");
+    client.user.setPresence({ game: { name: 'uwu' }, status: 'online' })
+});
+
+client.on("message", (message) => {
+
+    if(!message.author.bot) {
+        if(message.content.toLowerCase().includes('uwu')) {
+            message.channel.send('uwu');
+        }
+    }
+
+});
+
+client.login(config.token);

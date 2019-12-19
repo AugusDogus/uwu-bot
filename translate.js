e29bafe23c43da203393aa7d61092f096021c509
Augie <augie@luebbers.email>
2019-12-19 15:52:12 -0600

    uwu

--- translate.js
+++ translate.js
@@ -6,23 +6,20 @@ function translate(message) {
         if (message.content != null && typeof(message.content) === 'string') {
             let translation = message.content;
             // https://stackoverflow.com/questions/1144783/how-to-replace-all-occurrences-of-a-string
-            translation = translation.split('L').join('W');
-            translation = translation.split('R').join('W');
-            translation = translation.split('l').join('w');
-            translation = translation.split('r').join('w');
-            translation = translation.split('no').join('nyo');
-            translation = translation.split('mo').join('myo');
-            translation = translation.split('No').join('Nyo');
-            translation = translation.split('Mo').join('Myo');
-            translation = translation.split('na').join('nya');
-            translation = translation.split('na').join('nya');
-            translation = translation.split('ni').join('nyi');
-            translation = translation.split('nu').join('nyu');
-            translation = translation.split('ne').join('nye');
-            translation = translation.split('anye').join('ane');
-            translation = translation.split('inye').join('ine');
-            translation = translation.split('onye').join('one');
-            translation = translation.split('unye').join('une');
+            translation = translation.replace(/(?:r|l)/g, 'w');
+            translation = translation.replace(/(?:R|L)/g, 'W');
+            translation = translation.replace(/(?:no)/g, 'nyo');
+            translation = translation.replace(/(?:mo)/g, 'myo');
+            translation = translation.replace(/(?:No)/g, 'Nyo');
+            translation = translation.replace(/(?:Mo)/g, 'Myo');
+            translation = translation.replace(/(?:na)/g, 'nya');
+            translation = translation.replace(/(?:ni)/g, 'nyi');
+            translation = translation.replace(/(?:nu)/g, 'nyu');
+            translation = translation.replace(/(?:ne)/g, 'nye');
+            translation = translation.replace(/(?:anye)/g, 'ane');
+            translation = translation.replace(/(?:inye)/g, 'ine');
+            translation = translation.replace(/(?:onye)/g, 'one');
+            translation = translation.replace(/(?:unye)/g, 'une');
             if (message.content === translation) {
                 console.log(translation);
                 reject('Translated message was the same as original');

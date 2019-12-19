e0ccef264bbc06a2a4ad8fb5edfc63516c8b387d
Augie <augie@luebbers.email>
2019-12-19 15:40:55 -0600

    uwu

--- translate.js
+++ translate.js
@@ -5,22 +5,24 @@ function translate(message) {
     return new Promise((resolve, reject) => {
         if (message.content != null && typeof(message.content) === 'string') {
             let translation = message.content;
-            translation = translation.replace('L', 'W');
-            translation = translation.replace('R', 'W');
-            translation = translation.replace('l', 'w');
-            translation = translation.replace('r', 'w');
-            translation = translation.replace('no', 'nyo');
-            translation = translation.replace('mo', 'myo');
-            translation = translation.replace('No', 'Nyo');
-            translation = translation.replace('Mo', 'Myo');
-            translation = translation.replace('na', 'nya');
-            translation = translation.replace('ni', 'nyi');
-            translation = translation.replace('nu', 'nyu');
-            translation = translation.replace('ne', 'nye');
-            translation = translation.replace('anye', 'ane');
-            translation = translation.replace('inye', 'ine');
-            translation = translation.replace('onye', 'one');
-            translation = translation.replace('unye', 'une');
+            // https://stackoverflow.com/questions/1144783/how-to-replace-all-occurrences-of-a-string
+            translation = translation.split('L').join('W');
+            translation = translation.split('R').join('W');
+            translation = translation.split('l').join('w');
+            translation = translation.split('r').join('w');
+            translation = translation.split('no').join('nyo');
+            translation = translation.split('mo').join('myo');
+            translation = translation.split('No').join('Nyo');
+            translation = translation.split('Mo').join('Myo');
+            translation = translation.split('na').join('nya');
+            translation = translation.split('na').join('nya');
+            translation = translation.split('ni').join('nyi');
+            translation = translation.split('nu').join('nyu');
+            translation = translation.split('ne').join('nye');
+            translation = translation.split('anye').join('ane');
+            translation = translation.split('inye').join('ine');
+            translation = translation.split('onye').join('one');
+            translation = translation.split('unye').join('une');
             if (message.content === translation) {
                 console.log(translation);
                 reject('Translated message was the same as original');
